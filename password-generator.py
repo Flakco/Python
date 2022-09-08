d99273e1378061ed49f0a15c08cb98c3d0f8cc3f
Flakco <108195320+Flakco@users.noreply.github.com>
2022-09-08 14:40:44 -0300

    Update password-generator.py

--- password-generator.py
+++ password-generator.py
@@ -1,9 +1,9 @@
 import random
-caracteres = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%¨&*()_+/*\|<>;:"
+characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%¨&*()_+/*\|<>;:"
 password = ""
 a = 15
 for i in range(a):
-    password += str(random.choice(caracteres))
+    password += str(random.choice(characters))
 print(password)
 
      
